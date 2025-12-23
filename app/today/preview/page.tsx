7d3325308a964f915fc9414384c296dc2565bc0d
Andrey Eremichev <andrey.eremichev@gmail.com>
2025-12-23 20:19:42 +0100

    Remove Today preview route

--- app/today/preview/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/today/preview/page.tsx
-import TodayPreviewClient from "./TodayPreviewClient";
-
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
-export default function TodayPreviewPage() {
-  return <TodayPreviewClient />;
-}
